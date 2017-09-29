3b439c001828c0a1bf520da7d01c886f25a075a9
Leif Taylor <Leif.Taylor@actifio.com>
2017-09-29 16:53:18 -0400

    Final update to inventory for now

--- inv/host/ora12c1.py
+++
@@ -1,7 +0,0 @@
-from rf_inventory import get_host_variables
-
-variables = {'name': 'ora12c1', 'ip': '172.17.200.213', 'ssh_user': 'root', 'ssh_pass': '12!pass345', 'ssh_private_key_file': '', 'app': 'ora12hdb', 'app_type': 'Oracle', 'apps': ['/', 'ora12hdb'], 'app_list': ['/', 'ora12hdb'], 'app_exclude': ['/'], 'cluster_ip': '172.17.200.213', 'racnodelist': '172.17.200.213', 'oracle_user': 'oracle', 'oracle_pass': '12!pass345', 'oracle_sid': 'ora12hdb1', 'oracle_home': '/oracle/app/12.2.0/dbhome_1', 'oracle_path': '/oracle/app/12.2.0/dbhome_1/bin', 'po_databasesid': 'achild', 'po_username': 'oracle', 'po_orahome': '/oracle/app/12.2.0/dbhome_1', 'po_tnsadmindir': '/oracle/app/12.2.0/dbhome_1/network/admin', 'po_totalmemory': '800', 'po_sgapct': '70'}
-
-
-def get_variables(prepend=None, append=None, delimiter='.', base=variables):
-    return get_host_variables(base=base, prepend=prepend, append=append, delimiter=delimiter)
